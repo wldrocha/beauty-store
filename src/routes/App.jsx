fbeed7b1624de5436d59d774e9fd5635274c8a91
Wladimir Rocha <wld.rocha@gmail.com>
2022-04-06 17:56:29 -0400

    [FEAT] router

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Layout from '../containers/Layout'
 import Login from '../containers/Login'
+import RecoveryPassWord from '../containers/RecoveryPassWord'
 import '../styles/global.scss'
 
 const App = () => {
   return (
-    <Layout>
-      <Login />
-    </Layout>
+    <BrowserRouter>
+      <Switch>
+        <Layout>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/recovery-pass" component={RecoveryPassWord} />
+        </Layout>
+      </Switch>
+    </BrowserRouter>
   )
 }
 
