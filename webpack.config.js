77adce9c174cef72dc6d8bee1456dad839a17ccc
Wladimir Rocha <wld.rocha@gmail.com>
2022-04-06 17:32:38 -0400

    [REF] Sass or css

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.(css|scss)$/i,
         use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
