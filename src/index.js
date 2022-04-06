8ced89a0a0bbc23b3c76bada705cd6c74a0d9280
Wladimir Rocha <wld.rocha@gmail.com>
2022-04-06 18:23:11 -0400

    [FEAT] new on react 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
+import App from './routes/App'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const container = document.getElementById('app')
+
+const root = ReactDOM.createRoot(document.getElementById('app'))
+
+root.render(<App />)
