9de9e47802d3491ce745b37b3935bf0ed6cdbcea
Wladimir Rocha <wld.rocha@gmail.com>
2022-04-08 15:12:39 -0400

    [FEAT] rest pages atomic design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ProductList from '../containers/ProductList'
 
 const Home = () => {
   return (
-    <div><h1>
-        React beauty Shop</h1></div>
+    <>
+      <ProductList />
+    </>
   )
 }
 
