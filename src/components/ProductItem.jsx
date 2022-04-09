e8eb1088f8095a5d3f8f51370edd8f2231156935
Wladimir Rocha <wld.rocha@gmail.com>
2022-04-09 11:23:41 -0400

    [FEAT] show data

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,18 +2,25 @@ import React from 'react'
 import '@styles/ProductItem.scss'
 import cart from '@icons/bt_add_to_cart.svg'
 
-const ProductItem = (product) => {
-  console.log(product)
+const ProductItem = ({
+  product: {
+    description,
+    price,
+    title,
+    images: { 0: image },
+  },
+}) => {
+  console.log(title)
   return (
     <div className="ProductItem">
       <img
-        src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        src={image}
         alt=""
       />
       <div className="product-info">
         <div>
-          <p>$120,00</p>
-          <p>Bike</p>
+          <p>$ {price}</p>
+          <p>{title}</p>
         </div>
         <figure>
           <img src={cart} alt="cart icon" />
