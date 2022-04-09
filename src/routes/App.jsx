f846b39d226de13da923cf563a1d5e8d6341a92b
Wladimir Rocha <wld.rocha@gmail.com>
2022-04-09 08:05:15 -0400

    [FEA] add alias

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from '../containers/Layout';
+import Layout from '@containers/Layout';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import PasswordRecovery from '../pages/PasswordRecovery';
@@ -11,7 +11,7 @@ import CreateAccount from '../pages/CreateAccount';
 import Checkout from '../pages/Checkout';
 import Orders from '../pages/Orders';
 import NotFound from '../pages/NotFound';
-import '../styles/global.scss';
+import '@styles/global.scss';
 
 const App = () => {
   return (
