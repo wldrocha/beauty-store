d748c20fa8d6b965c5bf8b988b30f3d915fe1b8a
Wladimir Rocha <wld.rocha@gmail.com>
2022-04-10 22:20:11 -0400

    [FEAT] show add items and show total

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import OrderItem from '@components/OrderItem'
+import AppContext from '@context/AppContext'
 import '@styles/MyOrder.scss'
 
-import arrow from"@icons/flechita.svg"
+import arrow from '@icons/flechita.svg'
 
 const MyOrder = () => {
+  const {
+    state: { cart: cartItems },
+  } = useContext(AppContext)
+
+  const sumTotal = () => {
+    return cartItems.reduce((acc, item) => acc + item.price, 0)
+  }
+
   return (
     <aside className="MyOrder">
+      {cartItems.map(product => (
+        <OrderItem key={`orderItem-${product?.id}`} {...product} />
+      ))}
       <div className="title-container">
         <img src={arrow} alt="arrow" />
         <p className="title">My order</p>
       </div>
       <div className="my-order-content">
-        <OrderItem />
         <div className="order">
           <p>
             <span>Total</span>
           </p>
-          <p>$560.00</p>
+          <p>${sumTotal()}</p>
         </div>
         <button className="primary-button">Checkout</button>
       </div>
