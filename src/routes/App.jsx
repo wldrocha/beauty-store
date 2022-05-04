2fa3f85bbd6f7b03a6120429555b8f13436eb0cc
Wladimir Rocha <wld.rocha@gmail.com>
2022-05-04 17:05:25 -0400

    [HOTFIX] base name on routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   const initialState = useInitialState()
   return (
     <AppContext.Provider value={initialState}>
-      <BrowserRouter>
+      <BrowserRouter basename="https://wldrocha.github.io/beauty-store/">
         <Layout>
           <Routes>
             <Route index path="/" element={<Home />} />
