4dee32a62300b4a916d872746d7e1b12fba534cc
Wladimir Rocha <wld.rocha@gmail.com>
2022-05-08 09:40:38 -0400

    [FIX] change router

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MemoryRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Layout from '@containers/Layout'
 import Home from '@pages/Home'
 import Login from '@pages/Login'
@@ -19,7 +19,7 @@ const App = () => {
   const initialState = useInitialState()
   return (
     <AppContext.Provider value={initialState}>
-      <MemoryRouter >
+      <HashRouter >
         <Layout>
           <Routes>
             <Route index path="/" element={<Home />} />
@@ -34,7 +34,7 @@ const App = () => {
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
-      </MemoryRouter>
+      </HashRouter>
     </AppContext.Provider>
   )
 }
